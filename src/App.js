7a5a9e68b8d5e2058e6f7476ed6ea2ba6babc494
luckstrike <55028068+luckstrike@users.noreply.github.com>
2020-06-28 21:57:55 -0500

    Add files via upload
    Switched from top bar to a side bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import MainNav from './components/MainNav'
-import { Jumbotron } from 'react-bootstrap'
-
 import './App.css';
+import SideBar from './components/SideBar'
+
 
 function App() {
   return (
     <div className="App">
-      <MainNav />
-      <Jumbotron fluid>
-        <h1>A personal website!</h1>
-        <img src="meandrev-cropped.jpg" height="25%" width="25%"/>
-        <hr></hr>
-        <p>I wasn't angry in this photo, forgive me.</p>
-      </Jumbotron>
+      <SideBar/>
     </div>
   );
 }
